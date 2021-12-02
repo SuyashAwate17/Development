33c4dc481c5e283f8a6ebf5beb599492b731ce05
SuyashAwate17 <93419487+SuyashAwate17@users.noreply.github.com>
2021-12-02 20:31:48 +0530

    Add files via upload

---
+++ Program262.java
@@ -0,0 +1,50 @@
+// Program to sort the array using Insertion Sort
+
+import java.util.*;
+
+class Sorting
+{
+	public void InsertioSort(int Arr[])
+	{
+		int i = 0, j = 0, key = 0;
+		int size = Arr.length;
+
+		for(i = 1; i<size; i++)
+		{
+			key = Arr[i];
+			for(j = i-1; (j >= 0)&&(Arr[j]>key); j--)
+			{
+				Arr[j+1] = Arr[j];
+			}
+			Arr[j+1] = key;
+		}
+	}
+}
+
+class Program262
+{
+	public static void main(String args[])
+	{
+		Scanner sobj = new Scanner(System.in);
+
+		System.out.println("Enter the size of array");
+		int size = sobj.nextInt();
+
+		int Arr[] = new int[size];
+		System.out.println("Enter the elements");
+		for(int i=0; i<Arr.length; i++)
+		{
+			Arr[i] = sobj.nextInt();
+		}
+
+		Sorting obj = new Sorting();
+
+		obj.InsertioSort(Arr);
+		
+		System.out.println("Data after sorting");
+		for(int i=0; i<Arr.length; i++)
+		{
+			System.out.println(Arr[i]);
+		}
+	}
+}
